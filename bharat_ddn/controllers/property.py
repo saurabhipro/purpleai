5a8d24d199ea3121c0bcde1a99ea45aab533158b
saurabhipro <saurabh.mehrotrag@gmail.com>
2025-06-30 15:15:11 +0530

    S3 SSL Issue

--- bharat_ddn/controllers/property.py
+++ bharat_ddn/controllers/property.py
@@ -229,10 +229,9 @@ class PropertyDetailsAPI(http.Controller):
 
             s3_client = boto3.client(
                 's3',
-                aws_access_key_id=AWS_ACCESS_KEY,
+                aws_access_key_id=AWS_ACCESS_KEY,   
                 aws_secret_access_key=AWS_SECRET_KEY,
-                region_name=AWS_REGION,
-                verify=False  # ⚠️ Keep False only in dev
+                region_name=AWS_REGION
             )
 
             # ✅ Fix base64 padding
